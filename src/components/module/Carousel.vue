17c6da76689b406a5808376b80cc3c8c81270aec
wn150509 <wangn292@gmail.com>
2018-10-22 14:45:06 +0800

    commit carousel.vue

--- src/components/module/Carousel.vue
+++ src/components/module/Carousel.vue
@@ -12,8 +12,8 @@
                 @sliding-start="onSlideStart"
                 @sliding-end="onSlideEnd">
       <!-- Slides with image only -->
-      <b-carousel-slide v-for="image in imageList" :key="image.id"
-                        :img-src="image.url">
+      <b-carousel-slide v-for="image in imageList" :key="image.carouselId"
+                        :img-src="image.carouselUrl">
       </b-carousel-slide>
     </b-carousel>
   </div>
@@ -25,29 +25,16 @@
       return {
         slide: 0,
         sliding: null,
-        imageList: [
-          {
-            "id": 1,
-            "url": "https://upload.jianshu.io/admin_banners/web_images/4448/d9a0fd8f0622d2323161b68d2123c7f28837d28f.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
-          },
-          {
-            "id": 2,
-            "url": "https://upload.jianshu.io/admin_banners/web_images/4500/e20f8255334b480222e199011779eb8d0cdabd0e.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
-          },
-          {
-            "id": 3,
-            "url": "https://upload.jianshu.io/admin_banners/web_images/4504/8f6fbad5108d79f95a0af380416da37da8263251.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
-          },
-          {
-            "id": 4,
-            "url": "https://upload.jianshu.io/admin_banners/web_images/4501/572782ecf027e81f518edce85f98036e88beb290.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
-          },
-          {
-            "id": 5,
-            "url": "https://upload.jianshu.io/admin_banners/web_images/4502/25b524b6d1d21c508b7ca6a6a0a77ead48eed1b6.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
-          }
-        ]
+        imageList: []
       }
+    },
+    created(){
+      var that=this
+      this.$http
+        .get('http://localhost:8080/carousel/all')
+        .then(function (response) {
+          that.imageList=response.data.data
+        })
     },
     methods: {
       onSlideStart(slide) {
